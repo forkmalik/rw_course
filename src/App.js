fd6e55b619a4e7a8e278e1645429f5052081f417
Vlad Savchenko <polkamaraka@gmail.com>
2020-07-03 16:33:36 +0300

    Day 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {moviesData} from "./moviesData";
+import MovieItem from './MovieItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super ()
+
+    this.state = {
+      movies: moviesData
+    };
+  }
+
+  removeMovie = movie => {
+    const updateMovies = this.state.movies.filter(function(item) {
+      return item.id !== movie.id;
+    });
+    this.setState({
+      movies: updateMovies
+    });
+  }
+
+  render() {
+    return(
+      <div>{this.state.movies.map(movie => {
+       return (
+         <MovieItem key={movie.id} movie={movie} removeMovie={this.removeMovie}/>
+        )
+      })}</div>
+    )
+  }
 }
 
 export default App;
